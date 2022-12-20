f82733e92db0afa2596dcb404fa94332a54045d3
Tsepang Maseti <64875578+Tkmaseti@users.noreply.github.com>
2022-12-20 16:22:10 +0200

    Update db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // HOST: "localhost", PORT: 27017, DB: "FINE"
     url: "mongodb://localhost:27017/Fine"
-    // url: "mongodb+srv://Tsepang:%40Tresh2020@cluster0.5rodhln.mongodb.net/FINE"
+    // url: "mongodb+srv://Tsepang:@cluster0.5rodhln.mongodb.net/FINE"
     
 };
